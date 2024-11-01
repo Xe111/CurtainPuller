6dc522f5817a01bf98dc17d6d4bf58082e7f9944
xe111 <2073661764@qq.com>
2024-11-01 16:13:55 +0800

    comment added

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,13 @@
 4. configable wifi ? 
 */
 
+/*改进与完善：
+1. 用timer来代替delay，这样可以在拉起或拉下窗帘的时候做其他事情
+2. 可以配置改变连接的wifi
+3. 实现根据窗帘的状态来控制窗帘
+
+*/
+
 #pragma region Preparation
 
 #include <Arduino.h>
@@ -15,7 +22,7 @@
 
 char auth[] = "b06b42bba5c3";
 char ssid[] = "WIFI_C912";
-char pswd[] = "17705003103";
+char pswd[] = "17705003103";//配置wifi
 
 #pragma endregion Preparation
 
@@ -23,7 +30,7 @@ char pswd[] = "17705003103";
 class CurtainPuller
 {
 private:
-    enum PIN
+    enum PIN //tb6612 使用的引脚和nodeMCU的引脚对应关系
     {
         PWMA = A17,
         AIN2 = A16,
@@ -31,46 +38,46 @@ private:
         STBY = A14
     };
 
-    int time_pullup; // sec
-    double pulldown_coef;
-    char motion_state;    // h for halt,u for pullup,d for pulldn
-    double curtain_state; // 1 for fully closed, 0 for fully opened
-    bool use_led;
+    int time_pullup; // seconds，拉起窗帘所需时间
+    double pulldown_coef;//拉下窗帘乘的系数，因为拉下窗帘的速度比拉起窗帘的速度快
+    char motion_state;    // h for halt,u for pullup,d for pulldn，现在是拉起还是拉下，还是停止，用于防止同时操作
+    double curtain_state; // 1 for fully closed, 0 for fully opened，窗帘的状态，既打开的程度，因为没法直接测量窗帘的位置，所以用这个变量来模拟
+    bool use_led;//是否使用板载led灯
     struct Blk
     {
         BlinkerButton btn_up;
         BlinkerButton btn_dn;
         BlinkerButton btn_dbg;
 
-        Blk() : btn_up("btn_up"), btn_dn("btn_dn"), btn_dbg("btn_dbg") {}
+        Blk() : btn_up("btn_up"), btn_dn("btn_dn"), btn_dbg("btn_dbg") {}//初始化按钮
         ~Blk() = default;
 
     } blk;
 
     static CurtainPuller *instance;
 
-    void _btn_up(const String &state);
-    void _btn_dn(const String &state);
-    void _btn_dbg(const String &state);
-    static void _btn_up_wrapper(const String& state);
-    static void _btn_dn_wrapper(const String& state);
-    static void _btn_dbg_wrapper(const String& state);
+    void _btn_up(const String &state);//按钮拉起窗帘的回调函数
+    void _btn_dn(const String &state);//按钮拉下窗帘的回调函数
+    void _btn_dbg(const String &state);//按钮调试的回调函数
+    static void _btn_up_wrapper(const String& state);//按钮拉起窗帘的回调函数的包装函数，成员函数不能直接作为回调函数，所以需要一个包装函数
+    static void _btn_dn_wrapper(const String& state);//按钮拉下窗帘的回调函数的包装函数
+    static void _btn_dbg_wrapper(const String& state);//按钮调试的回调函数的包装函数
 
-    inline void halt();
-    inline void pullup();
-    inline void pulldn();
+    inline void halt();//停止窗帘
+    inline void pullup();//拉起窗帘
+    inline void pulldn();//拉下窗帘
 
-    void _delay(bool is_pullingup, double percentage = 1.0);
+    void _delay(bool is_pullingup, double percentage = 1.0);//延时函数，用于模拟窗帘拉起和拉下的时间，percentage是拉起或拉下的百分比
 
 public:
-    CurtainPuller() : blk(), time_pullup(90), pulldown_coef(0.9), motion_state('h'), curtain_state(1.0), use_led(true)
+    CurtainPuller() : blk(), time_pullup(90), pulldown_coef(0.9), motion_state('h'), curtain_state(1.0), use_led(true)//初始化
     {
         instance = this;
     }
     ~CurtainPuller() = default;
 
-    void init();
-    void attach();
+    void init();//CurtainPuller的初始化函数
+    void attach();//类的绑定按钮的回调函数
 };
 using CP = CurtainPuller;
 
@@ -81,7 +88,7 @@ CP cp;
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(115200);//波特率为115200
   BLINKER_DEBUG.stream(Serial);
   BLINKER_DEBUG.debugAll();
 
